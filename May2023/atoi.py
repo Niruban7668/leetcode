8cc4f5926a08f640632951c51cb0d7ce3eaf4350
Niruban7668 <94823404+Niruban7668@users.noreply.github.com>
2023-05-23 23:11:08 +0530

    9th day

---
+++ May2023/atoi.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip() 
+        if not s:
+            return 0
+        sign = 1
+        i = 0
+        if s[0] == '-':
+            sign = -1
+            i += 1
+        elif s[0] == '+':
+            i += 1
+        result = 0
+        while i < len(s) and s[i].isdigit():
+            result = result * 10 + int(s[i])
+            i += 1    
+        result *= sign
+        result = max(-2**31, min(result, 2**31 - 1))
+        
+        return result
